600774787e45f7d004ba2f3db5507937d456fbd2
Catherine Moreus <cmoreus@gmail.com>
2016-02-19 15:23:13 -0500

    committing my assignment2 code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
+##
+## These functions allow you to create a "special" matrix type that can cache
+## the inverse of the matrix.  
 
-## Write a short comment describing this function
+## This function creates a "special" matrix type that can cache it's inverse.
+## Note: It is assumed that the matrix is square and invertible
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inversematrix) i <<- inversematrix
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a "special" matrix defined by the
+## function makeCacheMatrix. It returns the inverse from cache if it is available,
+## otherwise it computes the inverse and caches the inverse it just computed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        ##message("returning cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <-  solve(data, ...)
+    x$setinverse(i)
+    i
 }
